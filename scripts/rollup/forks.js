4f24ab49b5de2fad4fd03bec3e845cd88e1599d4
b.abbasi <b.abbasi@DESKTOP-B92KM7C>
2019-04-14 15:05:21 +0430

    fix unused vars

--- scripts/rollup/forks.js
+++ scripts/rollup/forks.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const bundleTypes = require('./bundles').bundleTypes;
-const moduleTypes = require('./bundles').moduleTypes;
+// const bundleTypes = require('./bundles').bundleTypes;
+// const moduleTypes = require('./bundles').moduleTypes;
 
 // If you need to replace a file with another file for a specific environment,
 // add it to this list with the logic for choosing the right replacement.
